73eddd50a023640ba4000d5c9da445201d8ddfc6
ohyesnoyes <brothernoyes@gmail.com>
2018-05-21 20:54:17 -0600

    connect firebase to newsletter

--- main.js
+++ main.js
@@ -1,71 +1,51 @@
-const users = [];
-// var user = {
-//     name: '',
-//     email: '',
-//     comment:'',
-//     isSubscribed: false
-// }
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAXiOngasyk5KwHZ4RKUlp6KiaWDB2wNq8",
+    authDomain: "comohyesnoyesdb-7a196.firebaseapp.com",
+    databaseURL: "https://comohyesnoyesdb-7a196.firebaseio.com",
+    projectId: "comohyesnoyesdb-7a196",
+    storageBucket: "comohyesnoyesdb-7a196.appspot.com",
+    messagingSenderId: "177275647537"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  
+
+  function subscribeToNewsletter () {
+  
+  var newsletterUser = database.ref('newsletterUser/');
+  
+    var user = {
+    name: document.getElementById('newsletterName').value,
+    email: document.getElementById('newsletterEmail').value
+    };
+    newsletterUser.push(user);
+
+  console.log('User added');
+  // isSubscribed = true;
+  // users.push(user);
+  
+  }
+
+function cancelSubscribe (){
+    subscribeForm.style.display = "none";
+}
 
 const subscribe = document.getElementById('subscribe');
 subscribe.value = "Subscribe";
 subscribe.style.backgroundColor = "#455274";
 const subscribeForm = document.getElementById('subscribeForm');
-subscribeForm.style.visibility= "hidden";
+subscribeForm.style.display = "none";
 
 subscribe.addEventListener('click', () => {
-     if (subscribeForm.style.visibility === "hidden"){
-        subscribeForm.style.visibility = "visible";
+     if (subscribeForm.style.display === "none"){
+        subscribeForm.style.display = "block";
         subscribe.style.backgroundColor = "darkRed";
         subscribe.value = "Close";
     } else {
-        subscribeForm.style.visibility = "hidden";
+        subscribeForm.style.display = "none";
         subscribe.style.backgroundColor = "#455274";
         subscribe.value = "Subscribe";
     }
 });
-
-function subscribeToNewsletter (name, email) {
-    name = document.getElementById('newsletterName').value;
-    email = document.getElementById('newsletterEmail').value;
-    user = {
-        name: name,
-        email: email
-    }
-    console.log('User added: ' + user.name + ' ' + user.email);
-    isSubscribed = true;
-    users.push(user);
-    
-    
-    return user;
-
-    
-}
-
-// for (i=0; i<users.length; i+=1){
-//     console.log('Push to users[]: ' + users[i].name + ' ' + users[i].email);
-// }
-
-// window.addEventListener('change', () => {
-    
-// });
-
-
-
-// let subscribeToNewsletter = (name, email) =>{
-//     subscribe.name = prompt('what is your name?');
-//     subscribe.email = prompt('what is your email?');
-
-//     user = {
-//         name: name,
-//         email:email
-//     }
-
-//     return user;
-// }
-
-// var createNewUser = () => {
-    
-//     users.push(subsribeToNewsletter.user);
-
-//     console.log(newUser);
-// }
