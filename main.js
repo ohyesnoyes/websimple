f9a6b9390e3a3e92058629fd5d392096cfb27fb9
ohyesnoyes <brothernoyes@gmail.com>
2018-05-30 15:05:25 -0600

    change carousel to grid, add alert to comment, hook up comment

--- main.js
+++ main.js
@@ -11,9 +11,10 @@
   var database = firebase.database();
 
   /************** */
-$('.ctaMessage').append('Join my monthly newsletter for awesome updates, filled with poetry and musings.') 
+$('.ctaMessage').html('<hr class="my-2"><p>Join my monthly newsletter for awesome updates, filled with poetry and musings.</p>'); 
+
 /***** subscribe form ********* */
-$('#btnSubscribe').click(function subscribeToNewsletter () {
+$('#btnSubscribe').click(function (event) {
     
     var newsletterUser = database.ref('newsletterUser/');
         const date = new Date;
@@ -28,53 +29,40 @@ $('#btnSubscribe').click(function subscribeToNewsletter () {
         newsletterUser.push(user);
         $('#subscribeForm').hide(250);
         console.log('User added');
+        
+        // cta success message to subscribe
+        $('.ctaMessage')
+            .slideDown(250)
+            .html('<p class="alert alert-success">Thanks for joining!</p>')
+            .delay(2000)
+            .slideUp(250);
+
 });
 
 $('#subscribe').val('Subscribe');
 $('#subscribeForm').hide();
 
-$('#btnCancelSubscribe').click( () => $('#subscribeForm').hide());
+$('#btnCancelSubscribe').click( function() {
+    $('#subscribe').show();
+    $('#subscribeForm').toggle(250);
+    
+});
 $('#subscribe').click( function () {
     $('#subscribeForm').toggle(250);
-    $('#subscribe').toggleClass('btn-danger');
-   
-    if ($(this).val() == 'Subscribe') {
-        $(this).val("Close");
-    }
-    else {
-        $(this).val('Subscribe');
-    }
-     
+    $('#subscribe').hide();
 });
 
-/***** What I Do section ********/
-$(document).ready(function(){
-    // Activate Carousel
-    $("#myCarousel").carousel();
-    
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
-    });
-});
+const commentMessage = "Please let me know in the comment box which services you are interested in.";
 
+$('#commentMessage')
+    .html(commentMessage + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
+    .attr('class', 'alert alert-warning')
+    .on('click touch', function() {
+        $('#commentMessage').hide();
+    });
 
-$('#btnComment').click(()=> {
-    
+$('#btnComment').click(function (event) {
+
     var comment = database.ref('comments/');
         const date = new Date;
 
@@ -85,7 +73,20 @@ $('#btnComment').click(()=> {
         comment: $('#comment').val(),
         date: date.toLocaleDateString()
         };
-        // $('#subscribeMessage').show().slideDown(1000).delay(3000).slideUp();
         comment.push(user);
         console.log('Comment added');
+
+        
+        // add if successfully pushed
+        $('#commentMessage')
+            .slideDown(250)
+            .html('<p class="alert alert-success">Thanks for your comment!</p>')
+            .delay(3000)
+            .slideUp(250);
+
+        $('#commentFirstName').val('');
+        $('#commentLastName').val('');
+        $('#commentEmail').val('');
+        $('#comment').val('');
+        
 });
